32338ce6b6d02c98c13ddf629ccf4de5a5a6fbde
Maximilian Läpple <m.max32@yahoo.com>
2024-06-08 14:38:12 +0200

    Add post fuer login Controller

--- SOL 2 API/Controllers/UsersController.cs
+++ SOL 2 API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using SOL_2_API.Services;
 using MongoDB.Bson;
 using System.Threading.Tasks;
 using MongoDB.Driver;
+using Microsoft.AspNetCore.Identity.Data;
 
 namespace SOL_2_API.Controllers
 {
@@ -59,6 +60,21 @@ namespace SOL_2_API.Controllers
 
             return Ok(user);
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestApi loginRequest)
+        {
+            var filter = Builders<User>.Filter.Eq(u => u.Username, loginRequest.Username);
+            var existingUser = await _dbService.Users.Find(filter).FirstOrDefaultAsync();
+
+            if (existingUser == null || existingUser.Password != loginRequest.Password)
+            {
+                return Unauthorized(new { message = "Ungültiger Benutzername oder Passwort." });
+            }
+
+            return Ok(new { message = "Login erfolgreich!" });
+        }
+
     }
 }
 
---
+++ SOL 2 API/Models/LoginRequestApi.cs
@@ -0,0 +1,9 @@
+﻿namespace SOL_2_API.Models
+{
+    public class LoginRequestApi
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
