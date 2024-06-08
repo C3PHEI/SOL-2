5153b98d6c418acd9d089255340162c024a13d11
Maximilian Läpple <m.max32@yahoo.com>
2024-06-08 13:13:57 +0200

    Update UsersController.cs

--- SOL 2 API/Controllers/UsersController.cs
+++ SOL 2 API/Controllers/UsersController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SOL_2_API.Models;
 using SOL_2_API.Services;
-using MongoDB.Driver;
+using MongoDB.Bson;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace SOL_2_API.Controllers
 {
@@ -20,8 +21,44 @@ namespace SOL_2_API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllUsers()
         {
-            var users = await _dbService.Users.Find(_ => true).ToListAsync();
+            var users = await _dbService.Users.Find(_ => true)
+                .Project(u => new
+                {
+                    Id = u.Id.ToString(),
+                    Username = u.Username,
+                    Password = u.Password,
+                    Role = u.Role
+                })
+                .ToListAsync();
             return Ok(users);
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<IActionResult> GetUserById(string id)
+        {
+            if (!ObjectId.TryParse(id, out var objectId))
+            {
+                return BadRequest(new { message = "Ungültiges ID-Format." });
+            }
+
+            var filter = Builders<User>.Filter.Eq(u => u.Id, objectId);
+            var user = await _dbService.Users.Find(filter)
+                .Project(u => new
+                {
+                    Id = u.Id.ToString(),
+                    Username = u.Username,
+                    Password = u.Password,
+                    Role = u.Role
+                })
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
         }
     }
 }
+
